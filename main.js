9a376ac77b68e3742c282723049dacafd9f4937e
sagar hussain <160107305+simplegithubr@users.noreply.github.com>
2024-03-15 21:43:38 +0500

    45qution .
    100 Days of Coding Challenge !
    Day 1:
    Question # 2,3,4.

---
+++ main.js
@@ -0,0 +1,10 @@
+// //q2
+// //let personName: string= "kinza"
+// //console.log('hello', personName,'would you like to learn some Python today?');
+// //q3
+// let personName: string = "ZAINAB"
+/// console.log(personName. toLowerCase());
+// console.log(personName. toUpperCase());
+// console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
+//q4
+console.log("tony robbins one said", "seeting goals is the first step in turning the invisible into the visible'");
